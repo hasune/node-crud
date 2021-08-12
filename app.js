41e64c670c6d78713459e9a3fe3c8d75f0395b6a
Korea Freedom <jyy0047@gmail.com>
2021-08-12 21:26:31 +0900

    Docker Push to ghcr.io

--- app.js
+++ app.js
@@ -3,19 +3,19 @@ var express = require("express");
 var app = express();
 
 app.get("/", function(req, res) {
-  res.send("Get 리퀘스트를 보냈군요!!!");
+  res.send("Get 리퀘스트를 보냈군요!!! 오빠 존멋");
 });
 
 app.post("/", function(req, res) {
-  res.send("Post 리퀘스트를 보냈군요!!!");
+  res.send("Post 리퀘스트를 보냈군요!!! 도커 돌아가요?");
 });
 
 app.put("/", function(req, res) {
-  res.send("Put 리퀘스트를 보냈군요!!!!");
+  res.send("Put 리퀘스트를 보냈군요!!!! 깃액션 돌고 있나요?");
 });
 
 app.delete("/", function(req, res) {
-  res.send("Delete 리퀘스트를 보냈군요!!!");
+  res.send("Delete 리퀘스트를 보냈군요!!! 깃액션 푸쉬중임??");
 });
 
 app.listen(3000, function () {
